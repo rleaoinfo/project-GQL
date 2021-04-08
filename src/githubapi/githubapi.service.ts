4d6025b89dc5d27b2a980578c37516bf1bb1ab6d
Rafael <rleaoinfo@gmail.com>
2021-04-08 14:53:13 -0300

    tester criados e funcionais para o serviço.

--- src/githubapi/githubapi.service.ts
+++ src/githubapi/githubapi.service.ts
@@ -1,16 +1,16 @@
 import { HttpService, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { map } from 'rxjs/operators';
+import { GithubApiHttpClient } from './github-api-http-client';
 
 @Injectable()
 export class GithubapiService {
-    constructor(private http: HttpService,
-        private readonly configService: ConfigService,) { }
+    constructor(
+        private readonly githubApihttpclient : GithubApiHttpClient) { }
 
-    async getUser(user : string){
-        const apiGithub = this.configService.get('GITHUB_API');
-        const userGithub = await this.http.get(apiGithub + '/users/' + user).pipe(map(response => response.data)).toPromise();
-        const Filter = userGithub.map((item: { login: any; id: any; node_id: any; name: any; html_url: any; repos_url: any; updated_at: any; created_at: any; email: any; }) => ({
+    async getUser(user: string) {
+        const userGithub = await this.githubApihttpclient.getUser(user)
+        const Filter = [userGithub].map((item: { login: any; id: any; node_id: any; name: any; html_url: any; repos_url: any; updated_at: any; created_at: any; email: any; }) => ({
             login: item.login,
             id: item.id,
             node_id: item.node_id,
